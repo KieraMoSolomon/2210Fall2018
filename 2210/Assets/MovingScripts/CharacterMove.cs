2e849f0376ddd722d0218eb625e04adf96e255c1
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-08-28 21:48:20 -0600

    Got the character controller to move, doesn't stop though

---
+++ 2210/Assets/MovingScripts/CharacterMove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+
+	public float Speed;
+	public float Gravity;
+	
+	private Vector3 moveDirection;
+	
+	private CharacterController controller;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		controller = GetComponent<CharacterController>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Move(controller, transform);
+	}
+
+	void Move(CharacterController controller, Transform transform)
+	{
+		if (controller.isGrounded)
+		{
+			if (Input.GetKeyDown(KeyCode.RightArrow))
+			{
+				moveDirection.Set(1f, 0, 0);
+				moveDirection = transform.TransformDirection(moveDirection);
+				moveDirection *= Speed;
+			}
+
+			else if (Input.GetKeyDown(KeyCode.LeftArrow))
+			{
+				moveDirection.Set(-1f, 0, 0);
+				moveDirection = transform.TransformDirection(moveDirection);
+				moveDirection *= Speed;
+			}
+			
+		}
+
+		moveDirection.y -= Gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+}
